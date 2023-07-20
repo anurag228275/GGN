9d75e6dccd4d9ff8dfac4065396d0a7ca907b3a0
anurag228275 <127708570+anurag228275@users.noreply.github.com>
2023-07-20 11:43:56 +0600

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xaa0a9ed03b14f758fc556046937334ac98ff62eb";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
